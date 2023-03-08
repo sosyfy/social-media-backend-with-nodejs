163897f0ca35d583375e85f4b504ba3a1bb83ac6
sospeter <sospeterkaranja02@gmail.com>
2023-03-08 13:33:47 +0300

    Feat: forum controller

---
+++ src/controllers/forum.js
@@ -0,0 +1,124 @@
+const ForumPost = require('#/models/forum');
+// Create forum post 
+
+exports.createForumPost = async (req, res) => {
+    const { title, description, category } = req.body;
+    const creater = req.user.id;
+
+    try {
+        const forumPost = await ForumPost.create({ title, creater, description, category });
+        res.status(201).json(forumPost);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+
+// Edit forum post 
+
+exports.editForumPost = async (req, res) => {
+    const { id } = req.params;
+    const { title, description } = req.body;
+    try {
+        const forumPost = await ForumPost.findByIdAndUpdate(id, { title, description }, { new: true });
+        res.json(forumPost);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+// Like forum post 
+
+exports.likeForumPost = async (req, res) => {
+    const { id } = req.params; const userId = req.user._id;
+    try {
+        const forumPost = await ForumPost.findById(id);
+        if (!forumPost) {
+            return res.status(404).json({ error: 'Forum post not found.' });
+        }
+        let updatedForumPost;
+        const alreadyLiked = forumPost.likes.includes(userId);
+        if (alreadyLiked) {
+            updatedForumPost = await ForumPost.findByIdAndUpdate(id, { $pull: { likes: userId } }, { new: true });
+        } else {
+            updatedForumPost = await ForumPost.findByIdAndUpdate(id, { $push: { likes: userId } }, { new: true });
+        }
+
+        res.json(updatedForumPost);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Forum posts by category
+exports.getForumPostsByCategory = async (req, res) => {
+    const { category } = req.params;
+    const { page = 1 } = req.query;
+    const limit = 20;
+    const skip = (page - 1) * limit;
+    try {
+        const forumPosts = await ForumPost.find({ category })
+            .sort({ createdAt: -1 })
+            .skip(skip)
+            .limit(limit);
+
+        res.json(forumPosts);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+// Trending, most liked, latest forum posts
+
+exports.getFilteredForumPosts = async (req, res) => {
+    const { filter , category } = req.params;
+    const { page = 1 } = req.query;
+    const limit = 20;
+    const skip = (page - 1) * limit;
+    let sort;
+
+    switch (filter) {
+        case 'trending':
+            const pastFewHours = new Date(Date.now() - 1000 * 60 * 60 * 3); // Last 3 hours
+            sort = { likes: -1, createdAt: -1 };
+            const trendingForumPosts = await ForumPost.find({ createdAt: { $gte: pastFewHours } , category })
+                .sort(sort)
+                .skip(skip)
+                .limit(limit);
+            res.json(trendingForumPosts);
+            break;
+
+        case 'most-liked':
+            sort = { likes: -1, createdAt: -1 };
+            const mostLikedForumPosts = await ForumPost.find({ category })
+                .sort(sort)
+                .skip(skip)
+                .limit(limit);
+
+            res.json(mostLikedForumPosts);
+            break;
+
+        case 'latest':
+            sort = { createdAt: -1 };
+            const latestForumPosts = await ForumPost.find({ category })
+                .sort(sort)
+                .skip(skip)
+                .limit(limit);
+
+            res.json(latestForumPosts);
+            break;
+        default:
+            res.status(400).json({ error: 'Invalid filter.' });
+    }
+};
+
+
+// Increment views for forum post 
+
+exports.incrementForumPostViews = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const forumPost = await ForumPost.findByIdAndUpdate(id, { $inc: { views: 1 } }, { new: true });
+        res.json(forumPost);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
